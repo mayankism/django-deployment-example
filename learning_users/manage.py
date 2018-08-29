10af42511309af42e8860d1e06051d8ceaf91493
Mayank Bhandari <41547834+mayankism@users.noreply.github.com>
2018-08-29 20:38:33 +0530

    Update manage.py

--- learning_users/manage.py
+++ learning_users/manage.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_users.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
